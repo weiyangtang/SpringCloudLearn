e42b9edea6c1365fe41cfc3890528725fc983a4a
tangweiyang <702185070@qq.com>
2019-11-04 14:52:55 +0800

    Initial commit

---
+++ EurekaClient/src/main/java/EurekaDemo/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package EurekaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Auther: tangweiyang
+ * @Date: 2019/11/4 13:27
+ * @Description:
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
+
+
---
+++ EurekaServer/src/main/java/com/EurekaDemo/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.EurekaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: tangweiyang
+ * @Date: 2019/11/4 13:27
+ * @Description:
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
+
+
